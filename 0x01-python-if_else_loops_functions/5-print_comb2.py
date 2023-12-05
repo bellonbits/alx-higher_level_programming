94b6632099c9b0cf88fe6123e340f49e897c6adc
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-05 04:06:24 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print numbers from 0 to 99
-# demonstrates how to use a loop and string formatting to affect program output
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
 
 # For each number in range sequence
 for number in range(0, 99):
