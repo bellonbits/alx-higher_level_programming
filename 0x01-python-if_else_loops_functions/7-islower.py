e83091d3d64546cc93bc192c3b3401c00b3eb7a2
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-05 04:06:56 +0300

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to check for lowercase character using ASCII code
-# demonstrates how to use a function and if condition to affect program output
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
 
 def islower(c):
     """Checks for lowercase
