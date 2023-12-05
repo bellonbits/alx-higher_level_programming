e2396443e585d954889516864f85ec5bcb8b2c1b
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-05 04:06:09 +0300

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print all numbers from 0 to 98 in decimal and in
-# hexadecimal
-# demonstrates how to use a loop to affect program output
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
 
 # For each number in range sequence
 for number in range(99):
