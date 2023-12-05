63944eb57f459a22c1c047f810ba5af5ca17e6a0
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-05 03:48:08 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,13 +1,3 @@
-#!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to retrieve an element from a list like in C
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
-
 def element_at(my_list, idx):
     """Retrieves an element from a list like in C
 
