08cb342ff8c851548aad7c842df25f0fed438720
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-05 03:51:44 +0300

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to delete the item at a specific position in a list
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
 
 def delete_at(my_list=[], idx=0):
     """deletes the item at a specific position in a list
