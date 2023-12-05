e90aace69fa620929944650051c6288214d93bf5
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-05 03:50:21 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to removes all characters c and C from a string
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
 
 def no_c(my_string):
     """Removes all characters c and C from a string
