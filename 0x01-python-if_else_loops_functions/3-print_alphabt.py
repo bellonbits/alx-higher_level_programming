a35f518429c3b65b4853b797987552184d2f5af6
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-05 04:05:54 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print the ASCII alphabet in lowercase except 'q' and 'e'
-# demonstrates how to use a loop, a loop clause and an if condition to
-# affect program output
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
 
 # For each ascii_code in range sequence
 for ascii_code in range(97, 123):
