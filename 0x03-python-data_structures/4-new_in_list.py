792fb1f3055b81baf1c83a28659c7d4581374432
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-05 03:50:38 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to replace an element in a list at a specific position
-# without modifying the original list (like in C)
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
 
 def new_in_list(my_list, idx, element):
     """Replace an element in a list at a specific location
