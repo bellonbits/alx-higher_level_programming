3ec171da74b83a6e43e7ba7860f4c6b4ff52fb56
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-21 09:25:23 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    num_of_elements = 0
-    for index in range(x):
-        try:
-            print(my_list[index], end="")
-            num_of_elements += 1
-        except IndexError:
-            break
 
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
     print()
-    return num_of_elements
+    return printed
