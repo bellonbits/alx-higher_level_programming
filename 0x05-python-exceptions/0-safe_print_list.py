207e2cb2c56dc3fc21cd62184c4bc912d6b0610e
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2023-12-21 09:13:51 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print x elements of a list.
-# demonstrates how to use a try ... except for exception handling
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
-
 def safe_print_list(my_list=[], x=0):
     num_of_elements = 0
     for index in range(x):
