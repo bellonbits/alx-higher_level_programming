cb24d714c8d880af0cd6fc137cb7ef31d5f7cb7f
Peter Gatitu Mwangi <121729969+bellonbits@users.noreply.github.com>
2024-03-11 13:11:37 +0300

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/103-fast_alphabet.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-# Import the string module
-import string
-
-# Print out alphabets
-print(string.ascii_uppercase)
--- 0x02-python-import_modules/5-variable_load.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-# Import a from file
-from main import a
-
-# This code should not run when this file is imported
-if __name__ == "__main__":
-    print(a)
